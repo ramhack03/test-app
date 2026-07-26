296dc0efe9693157afb5bb95bf610ddd1d51803c
Ramkumar S <ajithram1112@gmail.comm>
2026-07-26 09:47:07 +0000

    Update Terraform S3 backend for new AWS account

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,14 +1,11 @@
 terraform {
   backend "s3" {
-<<<<<<< HEAD
-    bucket         = "ramkumar-terraform-state-891377344088"
-    key            = "sample-app/terraform.tfstate"
-    region         = "us-east-1"
-    dynamodb_table = "terraform-locks"
-    encrypt        = true
-  
+    bucket  = "ramkumar-terraform-state-122610514582"
+    key     = "sample-app/terraform.tfstate"
+    region  = "us-east-1"
+    encrypt = true
+
     # Enables native S3 locking (Requires Terraform 1.10+)
-    use_lockfile = true 
+    use_lockfile = true
   }
 }
->>>>>>> 0ada02dfe77f988446ec72ef56c24a4b5d33fb4a
