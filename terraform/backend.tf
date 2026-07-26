e7510545e4d76e84b4e1bf3161fafaa9e94e693e
ramhack03 <141500734+ramhack03@users.noreply.github.com>
2026-07-26 10:21:13 +0000

    Align Terraform backend with AWS state bucket

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,8 +1,5 @@
 terraform {
   backend "s3" {
-    bucket  = "ramkumar-terraform-state-122610514582"
-    key     = "sample-app/terraform.tfstate"
-    region  = "us-east-1"
     encrypt = true
     use_lockfile = true
   }
