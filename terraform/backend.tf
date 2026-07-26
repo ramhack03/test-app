d20b761e2fe638aabce922e5e9f284115ee4d81a
ramhack03 <141500734+ramhack03@users.noreply.github.com>
2026-07-26 08:46:07 +0000

    fix terraform s3 backend bucket configuration

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,5 +1,7 @@
 terraform {
   backend "s3" {
+    bucket       = "ramkumar-terraform-state-891377344088"
+    key          = "frontend-app/terraform.tfstate"
     key          = "frontend-app/terraform.tfstate"
     region       = "us-east-1"
     
